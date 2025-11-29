59885dc2d1cdab755fb9c06dbc3f2ee546e430f2
Giuliano-ehb <giuliano.schaerlaecken@student.ehb.be>
2025-11-29 16:08:38 +0100

    server met dummy code

---
+++ server.js
@@ -0,0 +1,27 @@
+// server.js (WORKING DUMMY VERSION)
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+app.use(express.json());
+
+// Dummy GET route
+app.get("/", (req, res) => {
+  res.send({ message: "Hello from dummy server!" });
+});
+
+// Another dummy route
+app.get("/test", (req, res) => {
+  res.send({ status: "ok", data: "This is just dummy data" });
+});
+
+// Dummy POST route
+app.post("/echo", (req, res) => {
+  res.send({ received: req.body });
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Dummy server running at http://localhost:${port}`);
+});
