51ce856ee902950c36aca76664beb4e408e3c18e
Giuliano-ehb <giuliano.schaerlaecken@student.ehb.be>
2025-12-26 15:26:37 +0100

    products route aangepast dat het daadwerkelijk kleren terugstuurd

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,16 @@
-const express = require("express"); const router = express.Router(); 
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/Products"); 
 
-router.get("/products", (req, res) => 
-
-{ res.json({ message: "Products is working" }); }); 
-
+router.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find(); 
+    res.json(products); 
+ 
+} catch (error) {
+   
+    res.status(500).json({ error: "cannot get products" });
+  }
+});
 
 module.exports = router;
