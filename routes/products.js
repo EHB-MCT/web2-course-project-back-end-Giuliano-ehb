66bb9e5235346ba7edf12d9f089641ab688c7c80
Giuliano-ehb <giuliano.schaerlaecken@student.ehb.be>
2025-12-26 16:46:12 +0100

    vergeten post aan te maken , kan nu kleren posten

--- routes/products.js
+++ routes/products.js
@@ -1,19 +1,31 @@
-const express = require("express"); 
-const router = express.Router(); 
-const Product = require("../models/Products"); 
-router.get("/products", async (req, res) => { 
-  
-  try { 
-    
-    const products = await Product.find(); res.json(products); 
-  
-  } catch (err) { 
-    
-    console.error(err); 
-    
-    res.status(500).json({ error: "cannot get products" }); 
-  
-  }}); 
-    
-    
-    module.exports = router;
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/Products");
+
+
+router.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: "cannot get products" });
+  }
+});
+
+
+
+
+router.post("/products", async (req, res) => {
+  try {
+
+
+    const newProduct = await Product.create(req.body);
+    res.status(201).json(newProduct);
+ 
+  } catch (error) {
+
+    res.status(500).json({ error: "cannot create products" });
+  }
+});
+
+module.exports = router;
