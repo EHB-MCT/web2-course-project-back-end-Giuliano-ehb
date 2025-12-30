6947a7a2f8f4e5f20e791692f296395362f949ba
Giuliano-ehb <giuliano.schaerlaecken@student.ehb.be>
2025-12-30 13:38:30 +0100

    outfit getten user

---
+++ routes/outfits.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const Outfits = require("../models/Outfits");
+const User = require("../models/Users"); 
+
+
+
+// outfit getten van user
+
+
+router.get("/outfits/:userId", async (req, res) => {
+  try {
+    const outfits = await Outfits.find({ userId: req.params.userId });
+    res.json(outfits);
+
+  } catch (error) {
+
+    res.status(500).json({ error: "cannot get outfits" });
+  }
+});
+
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(productRoutes);
+app.use("/", require("./routes/outfits"));
 connectDB();
 
 
